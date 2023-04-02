918faffd88775e0750f04ca90ab3f932e8b4492d
Hermes Feitoza Barros <57917124+WinterFB@users.noreply.github.com>
2023-04-01 22:59:46 -0300

    BM Atividade 2

---
+++ diversasVariaveis.java
@@ -0,0 +1,12 @@
+class diversasVariaveis
+{
+    public static void main(String[] args){
+        int variavel1 = 1;
+        long variavel2 = 2;
+        float variavel3 = 20.0f;
+        double variavel4 = 30.0;
+        boolean b = true;
+        char c = 'k';
+        System.out.println("Inteiro 32 bits: " + variavel1 + "\nInteiro 64 bits: "+ variavel2 + "\nFlutuante 32 bits: "+ variavel3 + "\nFlutuante 64 bits: "+ variavel4);
+    }
+}
